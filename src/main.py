ee9b34bf61bad9012e6edf3f81e5d0895764464a
Leon Sczepansky <leon.sczepansky@gmail.com>
2022-06-24 23:28:25 +0200

    update raspberri pi measurement service working

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 import time
-
+from w1thermsensor import W1ThermSensor, Unit
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from random import randrange
 from os import getenv
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+
+green_led = 20
+red_led = 16
+yellow_led = 26
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(red_led, GPIO.OUT)
+GPIO.setup(yellow_led, GPIO.OUT)
+GPIO.setup(green_led, GPIO.OUT)
+
+GPIO.output(red_led, GPIO.HIGH)
+GPIO.output(yellow_led, GPIO.HIGH)
+GPIO.output(green_led, GPIO.HIGH)
 
 load_dotenv()
 
@@ -13,28 +29,19 @@ client = MongoClient(MONGO_URI)
 
 # connect with bierbot collection
 db = client.bierbot
-
-mockdoc = {
-    "knoten": "thron1",
-    "temperature": "20",
-    "timestamp": "15:00:00",
-    "humidity": "50",
-    "weight": "400",
-    "gps": "52.5,13.5",
-    "battery": "100"
-}
-
-client.bierbot.measures.insert_one(mockdoc)
+w1_sensor = W1ThermSensor()
 
 while True:
-    mockdock = {
+    humidity, temp = Adafruit_DHT.read(11,21)
+    measurement = {
         "knoten": "thron1",
-        "temperature": randrange(20, 30),
+        "temperature1": temp,
+        "temperature2": w1_sensor.get_temperature(),
         "timestamp": time.strftime("%H:%M:%S"),
-        "humidity": randrange(50, 60),
+        "humidity": humidity,
         "weight": randrange(400, 500),
         "gps": "52.5,13.5",
         "battery": randrange(0, 100)
     }
-    client.bierbot.measures.insert_one(mockdock)
+    client.bierbot.measures.insert_one(measurement)
     time.sleep(10)
