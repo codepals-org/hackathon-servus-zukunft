b9c09c35b6b92a432abf4bba440e24d9140e1f27
vernaldaylight <Frank.Sarah@swm.de>
2022-06-25 11:19:54 +0200

    bugfixes

--- chatbot/rasa_project/actions/actions.py
+++ chatbot/rasa_project/actions/actions.py
@@ -142,3 +142,41 @@ class ActionQueryWeight(Action):
 
         print()
         return []
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted, EventType
+
+
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
+
+    @staticmethod
+    def fetch_slots(tracker: Tracker) -> List[EventType]:
+        """Collect slots that contain the user's name and phone number."""
+
+        slots = []
+        for key in ("name", "phone_number"):
+            value = tracker.get_slot(key)
+            if value is not None:
+                slots.append(SlotSet(key=key, value=value))
+        return slots
+
+    async def run(
+      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text=f"Willkommen. Möchtest du etwas bestellen?")
+        dispatcher.utter_message(buttons = [
+                    {"payload": '/order', "title": "Ja"},
+                    {"payload": "/no_wishes", "title": "Nein"},
+                ])
+
+        # the session should begin with a `session_started` event
+        events = [SessionStarted()]
+
+        # an `action_listen` should be added at the end as a user message follows
+        events.append(ActionExecuted("action_listen"))
+
+        return events
